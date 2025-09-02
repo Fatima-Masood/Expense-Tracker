de5c5924ede38862234df627020a1565b260a499
Sayyeda Fatima Masood <sfmasood13@gmail.com>
2025-09-02 15:14:47 +0500

    minor changes

--- next.config.mjs
+++ next.config.mjs
@@ -5,11 +5,10 @@ const nextConfig = {
     return [
       {
         source: "/:path*",
-        destination:
-          "https://expense-tracker-backend-production-724a.up.railway.app/:path*",
+        destination: "https://expense-tracker-backend-production-724a.up.railway.app/:path*",
       },
     ];
   },
 };
 
-module.exports = nextConfig;
+export default nextConfig;
