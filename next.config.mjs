8e0e645fe5dcc7fa4f3958cb7c5464a73b7c58cd
Sayyeda Fatima Masood <sfmasood13@gmail.com>
2025-09-02 16:24:21 +0500

    cors error resolved

--- next.config.mjs
+++ next.config.mjs
@@ -4,12 +4,8 @@ const nextConfig = {
   async rewrites() {
     return [
       {
-        source: "/api/:path*",
+        source: "/:path*",
         destination: "https://expense-tracker-backend-production-724a.up.railway.app/:path*",
-      },
-      {
-        source: "/oauth2/authorization/:path*",
-        destination: "https://expense-tracker-backend-production-724a.up.railway.app/oauth2/authorization/:path*",
       },
     ];
   },
